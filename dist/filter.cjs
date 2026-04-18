f1c1e847375217903dd97a29705a25995071b3b9
Brandon Sheehy - Hermes <sheehy.brandon@gmail.com>
2026-04-18 01:04:50 -0500

    perf(filter): replace Array.filter with while-loop implementation

--- dist/filter.cjs
+++ dist/filter.cjs
@@ -4,7 +4,18 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var curryRight = require('./curryRight.cjs');
 
-const filter = (arr, filterFunc) => arr.filter((val) => filterFunc(val));
+const filter = (arr, filterFunc) => {
+    const result = [];
+    let index = 0;
+    while (index < arr.length) {
+        const value = arr[index];
+        if (filterFunc(value)) {
+            result.push(value);
+        }
+        index += 1;
+    }
+    return result;
+};
 var filter$1 = /*#__PURE__*/ curryRight.default(filter, filter.length);
 
 exports.default = filter$1;
