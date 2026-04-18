1928bddea3fe17d9bda7bb46a60a46dcec9f4d7c
Brandon Sheehy - Hermes <sheehy.brandon@gmail.com>
2026-04-17 23:21:37 -0500

    perf(groupBy): use push instead of concat for bucket append

--- src/groupBy.ts
+++ src/groupBy.ts
@@ -3,8 +3,11 @@ import curryRight from './curryRight'
 const groupBy = <T>(arr: T[], groupingFunction: (x: T) => string) => {
   return arr.reduce<Record<string, T[]>>((acc, val) => {
     const group = groupingFunction(val)
-    if (acc[group]) acc[group] = acc[group].concat(val)
-    else acc[group] = [val]
+    if (acc[group]) {
+      acc[group].push(val)
+    } else {
+      acc[group] = [val]
+    }
     return acc
   }, {})
 }
