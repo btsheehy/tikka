4b608f2982994861133e52e598fcd5b6b42ae244
Brandon Sheehy <sheehy.brandon@gmail.com>
2026-04-27 15:28:47 -0500

    perf(curry): add arity fast paths for curry and curryRight

--- src/curry.ts
+++ src/curry.ts
@@ -27,23 +27,150 @@ type Curry3<A, B, C, R> = {
 function curry<A, B, R>(fn: (a: A, b: B) => R, arity?: number): Curry2<A, B, R>
 function curry<A, B, C, R>(fn: (a: A, b: B, c: C) => R, arity?: number): Curry3<A, B, C, R>
 function curry(fn: Function, arity = fn.length): Function {
-  const createCachedFunc = (existingArgs: unknown[] = []): Function => {
-    return (...args: unknown[]) => {
-      const currentArgs = existingArgs.concat(args)
+  const warnTooManyArguments = () => {
+    console.warn('Too many arguments passed to curried func.')
+  }
 
-      if (currentArgs.length >= arity) {
-        if (currentArgs.length > arity) {
-          console.warn('Too many arguments passed to curried func.')
-        }
-
-        return fn(...currentArgs.slice(0, arity))
+  if (arity === 1) {
+    return function f1(a: unknown) {
+      switch (arguments.length) {
+        case 0:
+          return f1
+        case 1:
+          return fn(a)
+        default:
+          warnTooManyArguments()
+          return fn(a)
       }
-
-      return createCachedFunc(currentArgs)
     }
   }
 
-  return createCachedFunc()
+  if (arity === 2) {
+    return function f2(a: unknown, b: unknown) {
+      switch (arguments.length) {
+        case 0:
+          return f2
+        case 1:
+          return function f1(nextB: unknown) {
+            switch (arguments.length) {
+              case 0:
+                return f1
+              case 1:
+                return fn(a, nextB)
+              default:
+                warnTooManyArguments()
+                return fn(a, nextB)
+            }
+          }
+        case 2:
+          return fn(a, b)
+        default:
+          warnTooManyArguments()
+          return fn(a, b)
+      }
+    }
+  }
+
+  if (arity === 3) {
+    return function f3(a: unknown, b: unknown, c: unknown) {
+      switch (arguments.length) {
+        case 0:
+          return f3
+        case 1:
+          return function f3_1(nextB: unknown, nextC: unknown) {
+            switch (arguments.length) {
+              case 0:
+                return f3_1
+              case 1:
+                return function f3_1_1(lastC: unknown) {
+                  switch (arguments.length) {
+                    case 0:
+                      return f3_1_1
+                    case 1:
+                      return fn(a, nextB, lastC)
+                    default:
+                      warnTooManyArguments()
+                      return fn(a, nextB, lastC)
+                  }
+                }
+              case 2:
+                return fn(a, nextB, nextC)
+              default:
+                warnTooManyArguments()
+                return fn(a, nextB, nextC)
+            }
+          }
+        case 2:
+          return function f3_2(nextC: unknown) {
+            switch (arguments.length) {
+              case 0:
+                return f3_2
+              case 1:
+                return fn(a, b, nextC)
+              default:
+                warnTooManyArguments()
+                return fn(a, b, nextC)
+            }
+          }
+        case 3:
+          return fn(a, b, c)
+        default:
+          warnTooManyArguments()
+          return fn(a, b, c)
+      }
+    }
+  }
+
+  const createCurried = (existingArgs: unknown[]): Function => {
+    return function curried(...args: unknown[]) {
+      if (args.length === 0) {
+        return curried
+      }
+
+      const totalLength = existingArgs.length + args.length
+
+      if (totalLength >= arity) {
+        if (totalLength > arity) {
+          warnTooManyArguments()
+        }
+
+        const finalArgs = new Array(arity)
+        let i = 0
+
+        while (i < existingArgs.length && i < arity) {
+          finalArgs[i] = existingArgs[i]
+          i += 1
+        }
+
+        let j = 0
+        while (i < arity) {
+          finalArgs[i] = args[j]
+          i += 1
+          j += 1
+        }
+
+        return fn(...finalArgs)
+      }
+
+      const nextArgs = new Array(totalLength)
+      let i = 0
+
+      while (i < existingArgs.length) {
+        nextArgs[i] = existingArgs[i]
+        i += 1
+      }
+
+      let j = 0
+      while (j < args.length) {
+        nextArgs[i + j] = args[j]
+        j += 1
+      }
+
+      return createCurried(nextArgs)
+    }
+  }
+
+  return createCurried([])
 }
 
 export default curry
--- src/curryRight.ts
+++ src/curryRight.ts
@@ -30,23 +30,149 @@ function curryRight<A, B, C, R>(
   arity?: number
 ): CurryRight3<A, B, C, R>
 function curryRight(fn: Function, arity = fn.length): Function {
-  const createCachedFunc = (existingArgs: unknown[] = []): Function => {
-    return (...args: unknown[]) => {
-      const currentArgs = existingArgs.concat(args)
+  const warnTooManyArguments = () => {
+    console.warn('Too many arguments passed to curried func.')
+  }
 
-      if (currentArgs.length >= arity) {
-        if (currentArgs.length > arity) {
-          console.warn('Too many arguments passed to curried func.')
-        }
-
-        return fn(...currentArgs.slice(0, arity).reverse())
+  if (arity === 1) {
+    return function f1(a: unknown) {
+      switch (arguments.length) {
+        case 0:
+          return f1
+        case 1:
+          return fn(a)
+        default:
+          warnTooManyArguments()
+          return fn(a)
       }
-
-      return createCachedFunc(currentArgs)
     }
   }
 
-  return createCachedFunc()
+  if (arity === 2) {
+    return function f2(b: unknown, a: unknown) {
+      switch (arguments.length) {
+        case 0:
+          return f2
+        case 1:
+          return function f1(nextA: unknown) {
+            switch (arguments.length) {
+              case 0:
+                return f1
+              case 1:
+                return fn(nextA, b)
+              default:
+                warnTooManyArguments()
+                return fn(nextA, b)
+            }
+          }
+        case 2:
+          return fn(a, b)
+        default:
+          warnTooManyArguments()
+          return fn(a, b)
+      }
+    }
+  }
+
+  if (arity === 3) {
+    return function f3(c: unknown, b: unknown, a: unknown) {
+      switch (arguments.length) {
+        case 0:
+          return f3
+        case 1:
+          return function f3_1(nextB: unknown, nextA: unknown) {
+            switch (arguments.length) {
+              case 0:
+                return f3_1
+              case 1:
+                return function f3_1_1(lastA: unknown) {
+                  switch (arguments.length) {
+                    case 0:
+                      return f3_1_1
+                    case 1:
+                      return fn(lastA, nextB, c)
+                    default:
+                      warnTooManyArguments()
+                      return fn(lastA, nextB, c)
+                  }
+                }
+              case 2:
+                return fn(nextA, nextB, c)
+              default:
+                warnTooManyArguments()
+                return fn(nextA, nextB, c)
+            }
+          }
+        case 2:
+          return function f3_2(nextA: unknown) {
+            switch (arguments.length) {
+              case 0:
+                return f3_2
+              case 1:
+                return fn(nextA, b, c)
+              default:
+                warnTooManyArguments()
+                return fn(nextA, b, c)
+            }
+          }
+        case 3:
+          return fn(a, b, c)
+        default:
+          warnTooManyArguments()
+          return fn(a, b, c)
+      }
+    }
+  }
+
+  const createCurried = (existingArgs: unknown[]): Function => {
+    return function curried(...args: unknown[]) {
+      if (args.length === 0) {
+        return curried
+      }
+
+      const totalLength = existingArgs.length + args.length
+
+      if (totalLength >= arity) {
+        if (totalLength > arity) {
+          warnTooManyArguments()
+        }
+
+        const finalArgs = new Array(arity)
+        let readIndex = arity - 1
+        let writeIndex = 0
+
+        while (readIndex >= 0) {
+          if (readIndex < existingArgs.length) {
+            finalArgs[writeIndex] = existingArgs[readIndex]
+          } else {
+            finalArgs[writeIndex] = args[readIndex - existingArgs.length]
+          }
+          readIndex -= 1
+          writeIndex += 1
+        }
+
+        return fn(...finalArgs)
+      }
+
+      const nextArgs = new Array(totalLength)
+      let i = 0
+
+      while (i < existingArgs.length) {
+        nextArgs[i] = existingArgs[i]
+        i += 1
+      }
+
+      let j = 0
+      while (j < args.length) {
+        nextArgs[i + j] = args[j]
+        j += 1
+      }
+
+      return createCurried(nextArgs)
+    }
+  }
+
+  return createCurried([])
 }
 
 export default curryRight
